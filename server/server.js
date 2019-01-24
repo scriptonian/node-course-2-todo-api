679c58ca7a9d671f811065d2b6be944e70bb2c3d
I-am Konstantin <scriptonian@gmail.com>
2019-01-24 06:09:44 -0500

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
+const {ObjectID} = require('mongodb');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
@@ -30,6 +31,26 @@ app.get('/todos', (req, res) => {
             })
         }, (e) => {
             res.status(400).send(e);
+        });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id)
+        .then((todo) => {
+            //id is valid by object is just not in the collection
+            if(!todo) {
+                return res.status(404).send();
+            }
+            res.send({todo})
+        })
+        .catch((e) => {
+            res.status(404).send();
         });
 });
 
